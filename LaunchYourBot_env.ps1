6823353fcbcb3c371fd10e09fefdeb3a52517149
geekgit <geeekz@gmail.com>
2017-10-15 07:38:53 +0000

    New environment variable

--- LaunchYourBot_env.ps1
+++ LaunchYourBot_env.ps1
@@ -1,4 +1,5 @@
 $TelegramBotName=(Get-ChildItem Env:TELEGRAM_BOT_NAME).value
+$TelegramBotType=(Get-ChildItem Env:TELEGRAM_BOT_TYPE).value
 $TelegramBotToken=(Get-ChildItem Env:TELEGRAM_BOT_TOKEN).value
 
-sbt "project examples" "run $TelegramBotName $TelegramBotToken"
+sbt "project examples" "run $TelegramBotType $TelegramBotToken"
